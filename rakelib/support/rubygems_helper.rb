09e1c30d0a65ddbd2c70052bcc3fe0822e1a74e9
Nick LaMuro <nicklamuro@gmail.com>
2019-10-16 20:12:16 -0500

    [RubygemsHelper] Add .gem_credentials
    Used for generating a `~/.gem/credentials` file when GEM_HOST_API_KEY is
    present as an ENV var.

--- rakelib/support/rubygems_helper.rb
+++ rakelib/support/rubygems_helper.rb
@@ -9,4 +9,19 @@ module RubygemsHelper
 
     Gem::GemRunner.new.run args
   end
+
+  # Create a gem_credentials file if the GEM_HOST_API_KEY ENV var is set
+  def gem_credentials creds = nil
+    if ENV["GEM_HOST_API_KEY"]
+      require 'yaml'
+
+      creds ||= { :rubygems_api_key => ENV["GEM_HOST_API_KEY"] }
+
+      mkdir_p File.dirname(Gem.configurations.credentials_path)
+      touch   Gem.configurations.credentials_path
+      chmod   0600, Gem.configurations.credentials_path
+
+      File.write Gem.configurations.credentials_path, creds.to_yaml
+    end
+  end
 end
